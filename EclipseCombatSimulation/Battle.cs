5536ec133427e90dd0943bbd2d9ce84c8c493f47
Dan Rausch <dan.rausch@gmail.com>
2013-08-18 20:04:47 -0400

    Created a new Battle class and moved code into the new class.

---
+++ EclipseCombatSimulation/Battle.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EclipseCombatSimulation
+{
+    class Battle
+    {
+        Random rnd = new Random();
+        int m_numBattles = 100000;
+        Fleet attackerFleet;
+        Fleet defenderFleet;
+
+        public class ShipDefinition
+        {
+            public List<Upgrade> m_interceptorUpgrades = new List<Upgrade>();
+            public int m_numInterceptors = 0;
+            public List<Upgrade> m_cruiserUpgrades = new List<Upgrade>();
+            public int m_numCruisers = 0;
+            public List<Upgrade> m_dreadnaughtUpgrades = new List<Upgrade>();
+            public int m_numDreadnaughts = 0;
+            public List<Upgrade> m_orbitalUpgrades = new List<Upgrade>();
+            public int m_numOrbitals = 0;
+            public int m_numAncients = 0;
+            public int m_numCenterBases = 0;
+        }
+
+        public Battle(ShipDefinition attackerShips, ShipDefinition defenderShips)
+        {
+            attackerFleet = CreateFleet(attackerShips);
+            defenderFleet = CreateFleet(defenderShips);
+        }
+
+        private Fleet CreateFleet(ShipDefinition ships)
+        {
+            Fleet fleet = new Fleet();
+            for (int i = 0; i < ships.m_numInterceptors; i++)
+            {
+                Interceptor interceptor;
+                if (ships.m_interceptorUpgrades.Count > 0)
+                {
+                    interceptor = new Interceptor(ships.m_interceptorUpgrades);
+                }
+                else
+                {
+                    interceptor = new Interceptor();
+                }
+                fleet.AddShip(interceptor);
+            }
+
+            for (int i = 0; i < ships.m_numCruisers; i++)
+            {
+                Cruiser cruiser;
+                if (ships.m_cruiserUpgrades.Count > 0)
+                {
+                    cruiser = new Cruiser(ships.m_cruiserUpgrades);
+                }
+                else
+                {
+                    cruiser = new Cruiser();
+                }
+                fleet.AddShip(cruiser);
+            }
+
+            for (int i = 0; i < ships.m_numDreadnaughts; i++)
+            {
+                Dreadnaught dreadnaught;
+                if (ships.m_dreadnaughtUpgrades.Count > 0)
+                {
+                    dreadnaught = new Dreadnaught(ships.m_dreadnaughtUpgrades);
+                }
+                else
+                {
+                    dreadnaught = new Dreadnaught();
+                }
+                fleet.AddShip(dreadnaught);
+            }
+
+            for (int i = 0; i < ships.m_numOrbitals; i++)
+            {
+                Orbital orbital;
+                if (ships.m_orbitalUpgrades.Count > 0)
+                {
+                    orbital = new Orbital(ships.m_orbitalUpgrades);
+                }
+                else
+                {
+                    orbital = new Orbital();
+                }
+                fleet.AddShip(orbital);
+            }
+
+            for (int i = 0; i < ships.m_numAncients; i++)
+            {
+                Ancient ancient = new Ancient();
+                fleet.AddShip(ancient);
+            }
+
+            for (int i = 0; i < ships.m_numCenterBases; i++)
+            {
+                CenterBase centerBase = new CenterBase();
+                fleet.AddShip(centerBase);
+            }
+
+            return fleet;
+        }
+
+        public void Run()
+        {
+            int defenderWins = 0;
+            int attackerWins = 0;
+
+            for (int i = 0; i < m_numBattles; i++)
+            {
+                if (Combat(attackerFleet, defenderFleet))
+                {
+                    attackerWins++;
+                }
+                else
+                {
+                    defenderWins++;
+                }
+
+                attackerFleet.ResetBattle();
+                defenderFleet.ResetBattle();
+            }
+
+            double winPercentage = attackerWins / (double)m_numBattles;
+            Console.Write("Winning percentage = " + winPercentage.ToString());
+        }
+
+        public bool Combat(Fleet attackers, Fleet defenders)
+        {
+            while (!attackers.Destroyed() && !defenders.Destroyed())
+            {
+                if (attackers.GetCurrentInitiative() > defenders.GetCurrentInitiative())
+                {
+                    Ship ship = attackers.GetAttacker();
+                    AttackFleet(ship, defenders);
+                }
+                else
+                {
+                    Ship ship = defenders.GetAttacker();
+                    AttackFleet(ship, attackers);
+                }
+
+                if (defenders.Done() && attackers.Done())
+                {
+                    defenders.ResetTurn();
+                    attackers.ResetTurn();
+                }
+            }
+
+            if (defenders.Destroyed())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void AttackFleet(Ship ship, Fleet fleet)
+        {
+            for (int i = 0; i < ship.RedDice; i++)
+            {
+                if (fleet.DetermineDamage(4, DiceRoll(), ship.Computers))
+                {
+                    if (fleet.Destroyed())
+                    {
+                        return;
+                    }
+                }
+            }
+            for (int i = 0; i < ship.OrangeDice; i++)
+            {
+                if (fleet.DetermineDamage(2, DiceRoll(), ship.Computers))
+                {
+                    if (fleet.Destroyed())
+                    {
+                        return;
+                    }
+                }
+            }
+            for (int i = 0; i < ship.YellowDice; i++)
+            {
+                if (fleet.DetermineDamage(1, DiceRoll(), ship.Computers))
+                {
+                    if (fleet.Destroyed())
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+
+        private int DiceRoll()
+        {
+            return rnd.Next(1, 7);
+        }
+    }
+}
--- EclipseCombatSimulation/Program.cs
+++ EclipseCombatSimulation/Program.cs
@@ -7,129 +7,13 @@ namespace EclipseCombatSimulation
 {
     class Program
     {
-        Random rnd = new Random();
         static void Main(string[] args)
         {
-            List<Ship> attackerList = new List<Ship>();
-            List<Ship> defenderList = new List<Ship>();
+            Battle.ShipDefinition def = new Battle.ShipDefinition();
+            def.m_numInterceptors = 1;
 
-            attackerList.Add(new Interceptor());
-            //attackerList.Add(new Interceptor());
-            //attackerList.Add(new Interceptor());
-
-            //defenderList.Add(new Ancient());
-            defenderList.Add(new Interceptor());
-            //defenderList.Add(new Interceptor());
-
-            int defenseWins = 0;
-            int offenseWins = 0;
-            int numBattles = 100000;
-
-            Program prog = new Program();
-
-            Fleet fleet1 = new Fleet(attackerList);
-            Fleet fleet2 = new Fleet(defenderList);
-
-            for (int i = 0; i < numBattles; i++)
-            {
-                if (prog.Combat(fleet1, fleet2))
-                {
-                    offenseWins++;
-                }
-                else
-                {
-                    defenseWins++;
-                }
-
-                fleet1.ResetBattle();
-                fleet2.ResetBattle();
-            }
-
-            double winPercentage = offenseWins / (double)numBattles;
-            Console.Write("Winning percentage = " + winPercentage.ToString());
-
-            winPercentage = offenseWins / 50;
-        }
-
-        public bool Combat(Fleet attackers, Fleet defenders)
-        {
-            while (!attackers.Destroyed() && !defenders.Destroyed())
-            {
-                if (attackers.GetCurrentInitiative() > defenders.GetCurrentInitiative())
-                {
-                    Ship ship = attackers.GetAttacker();
-                    AttackFleet(ship, defenders);
-                    //Console.WriteLine("Attacker ship attacks. Defender Fleet Status:");
-                    //defenders.PrintStatus();
-                }
-                else
-                {
-                    Ship ship = defenders.GetAttacker();
-                    AttackFleet(ship, attackers);
-                    //Console.WriteLine("Defender ship attacks. Attacker Fleet Status:");
-                    //attackers.PrintStatus();
-                }
-
-                if (defenders.Done() && attackers.Done())
-                {
-                    //Console.WriteLine("New Turn!!!");
-                    //Console.WriteLine("Attacker Fleet Status:");
-                    //attackers.PrintStatus();
-                    //Console.WriteLine("Defender Fleet Status:");
-                    //defenders.PrintStatus();
-
-                    defenders.ResetTurn();
-                    attackers.ResetTurn();
-                }
-            }
-
-            if (defenders.Destroyed())
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        private void AttackFleet(Ship ship, Fleet fleet)
-        {
-            for (int i = 0; i < ship.RedDice; i++)
-            {
-                if (fleet.DetermineDamage(4, DiceRoll(), ship.Computers))
-                {
-                    if (fleet.Destroyed())
-                    {
-                        return;
-                    }
-                }
-            }
-            for (int i = 0; i < ship.OrangeDice; i++)
-            {
-                if (fleet.DetermineDamage(2, DiceRoll(), ship.Computers))
-                {
-                    if (fleet.Destroyed())
-                    {
-                        return;
-                    }
-                }
-            }
-            for (int i = 0; i < ship.YellowDice; i++)
-            {
-                if (fleet.DetermineDamage(1, DiceRoll(), ship.Computers))
-                {
-                    if (fleet.Destroyed())
-                    {
-                        return;
-                    }
-                }
-            }
-        }
-
-        private int DiceRoll()
-        {
-            return rnd.Next(1, 7);
+            Battle battle = new Battle(def, def);
+            battle.Run();
         }
     }
 }
